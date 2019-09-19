728f28410c23dce0f3f9b063a811001d9a64caf3
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-09-18 22:35:53 -0600

    6502 virtual memory fits on stack just fine

--- 6502sim.c
+++ 6502sim.c
@@ -5,7 +5,7 @@
 
 #define START_ADDR 0x8000
 
-size_t sizeof_bin_file(FILE *fp) {
+static size_t sizeof_bin_file(FILE *fp) {
     size_t sz;
     if (fseek(fp, 0L, SEEK_END) != 0) {
         fprintf(stderr, "failed to seek");
@@ -15,20 +15,10 @@ size_t sizeof_bin_file(FILE *fp) {
     return sz;
 }
 
-void *malloc_or_exit(size_t sz) {
-    void *buf = malloc(sz);
-    if (buf == NULL) {
-        fprintf(stderr, "out of memory\n");
-        exit(EXIT_FAILURE);
-    } else {
-        return buf;
-    }
-}
-
 int main(int argc, char **argv) {
-    size_t prog_size; /* size in bytes of the program to run on emulator */
-    char *memory;     /* virtual memory of 6502 */
-    char *filename;   /* name of binary file to run */
+    size_t prog_size;    /* size in bytes of the program to run on emulator */
+    char memory[0xffff]; /* virtual memory of 6502 */
+    char *filename;      /* name of binary file to run */
     FILE *bin_file;
     size_t i;
 
@@ -44,8 +34,6 @@ int main(int argc, char **argv) {
     bin_file = fopen(filename, "rb");
     prog_size = sizeof_bin_file(bin_file);
 
-    memory = malloc_or_exit(0xFFFF);
-
     if (fread(memory + START_ADDR, 1, prog_size, bin_file) != prog_size) {
         fprintf(stderr,
             "%s: error: failed to read file: %s\n",
@@ -59,8 +47,6 @@ int main(int argc, char **argv) {
     for (i = START_ADDR; i < prog_size + START_ADDR; i++) {
         printf("%04lx:\t%02x\n", i, 0xFF & memory[i]);
     }
-
-    free(memory);
 
     return 0;
 }
